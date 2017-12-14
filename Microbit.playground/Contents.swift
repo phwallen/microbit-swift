0c0a76011c4d8b1a71e501755219a43fa3263bd6
Peter Wallen <peterwallen@Development-MacBook-Pro.local>
2017-12-14 15:40:16 +0000

    first commit

--- Microbit.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-//: A UIKit based Playground for presenting user interface test
-  
-import PlaygroundSupport
-
-let vc = MicrobitUIController()
-vc.microbit = Microbit("BBC micro:bit [tizip]")
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = vc
